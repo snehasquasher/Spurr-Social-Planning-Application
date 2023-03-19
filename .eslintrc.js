bcfb6e98ae3dacce36486391541a62e39d7d2f0d
Sneha <sneha.squasher@gmail.com>
2023-03-19 18:16:10 +0800

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,19 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "@typescript-eslint"
     ],
     "rules": {
     }
